376a88addc49674a40984fe275d8089634a11aa4
minglangdu <mldu@cydu.net>
2026-08-09 15:02:40 -0700

    add pearl code

--- core/src/main/java/com/bobapuyo/Board.java
+++ core/src/main/java/com/bobapuyo/Board.java
@@ -1,13 +1,24 @@
 package com.bobapuyo;
 
+import java.util.ArrayList;
+
 public class Board {
     private Pearl[] active; // currently falling or being placed pearls
     private int[][] pearls; // already placed pearls
     int phase;
 
+    private ArrayList<Pearl[]> next;
+
+    Pearl center, side;
+    int dir; // direction of side pearl
+
     public void update() {
         switch (phase) {
             case 0:
+                center = next.get(0)[0]; side = next.get(0)[1];
+                dir = 0; // side on top -> 0
+                
+                next.add(nextPearl());
                 // dropping blocks
                 break;
             case 1:
@@ -23,18 +34,44 @@ public class Board {
 
     }
 
+    private Pearl[] nextPearl() {
+        // first (center) on bottom
+        return new Pearl[] {new Pearl(2, 10, (int)Math.floor(Math.random() * 4)),
+        new Pearl(2, 11, (int)Math.floor(Math.random() * 4))};
+    }
+
     public Board() {
         pearls = new int[12][6];
         phase = 0;
+
+        for (int i = 0; i < Constants.NEXT_SIZE; i ++) {
+            next.add(nextPearl());
+        }
     }
 }
 
 class Pearl {
     int x; float y;
     float dy;
-    public Pearl(int x, int y) {
+    int color;
+
+    public boolean update(int[][] pearls) {
+        boolean collision = false;
+        float ny = y - dy;
+        for (int cy = (int)Math.floor(y); cy > Math.max(ny, 0); cy --) {
+            if (pearls[cy][x] != 0) {
+                collision = true;
+                ny = cy;
+            }
+        }
+        y = ny;
+        return collision;
+    }
+
+    public Pearl(int x, int y, int c) {
         this.x = x;
         this.y = (float)y;
         this.dy = 0.0f;
+        this.color = c;
     }
 }
--- core/src/main/java/com/bobapuyo/Constants.java
+++ core/src/main/java/com/bobapuyo/Constants.java
@@ -3,5 +3,6 @@ package com.bobapuyo;
 import com.badlogic.gdx.graphics.Color;
 
 public class Constants {
-
+    final public static int COLORS = 4;
+    final public static int NEXT_SIZE = 2;
 }
