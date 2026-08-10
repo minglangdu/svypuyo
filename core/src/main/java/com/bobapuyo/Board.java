6d1e66e830d3e85a600f0798e7206f293fc5d60b
minglangdu <mldu@cydu.net>
2026-08-09 21:26:49 -0700

    add falling and phases and collision

--- core/src/main/java/com/bobapuyo/Board.java
+++ core/src/main/java/com/bobapuyo/Board.java
@@ -1,9 +1,12 @@
 package com.bobapuyo;
 
+import com.badlogic.gdx.math.Vector2;
+
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Board {
-    private Pearl[] active; // currently falling or being placed pearls
+    private ArrayList<Pearl> active; // currently falling or being placed pearls
     private int[][] pearls; // already placed pearls
     int phase;
 
@@ -15,17 +18,46 @@ public class Board {
     public void update() {
         switch (phase) {
             case 0:
-                center = next.get(0)[0]; side = next.get(0)[1];
-                dir = 0; // side on top -> 0
-                
-                next.add(nextPearl());
                 // dropping blocks
+                center.setDy(Constants.GRAVITY);
+                side.setDy(Constants.GRAVITY);
+                // check for hitting bottom
+                Pearl lower, upper;
+                if (center.getXY()[1] < side.getXY()[1]) {
+                    lower = center; upper = side;
+                } else {
+                    lower = side; upper = center;
+                }
+                if (lower.update(pearls)) {
+                    phase = 2;
+                    setPearl(lower);
+                }
+                if (upper.update(pearls)) {
+                    phase = 2;
+                    setPearl(upper);
+                }
                 break;
             case 1:
                 // popping groups
+                ArrayList<Vector2> pops = getPops();
+                if (pops.size() == 0) {
+                    phase = 0;
+                }
                 break;
             case 2:
-                // checking for freefall
+                // freefall
+                if (active.size() == 0) {
+                    phase = 1;
+                }
+                for (int i = 0; i < active.size(); i ++) {
+                    Pearl cur = active.get(i);
+                    cur.setDy(cur.getDy() + Constants.GRAV_ACC);
+                    if (cur.update(pearls)) {
+                        setPearl(cur);
+                        active.remove(i);
+                        i --;
+                    }
+                }
                 break;
         }
     }
@@ -34,10 +66,25 @@ public class Board {
 
     }
 
+    private void setPearl(Pearl p) {
+        pearls[(int)p.getXY()[1]][(int)p.getXY()[0]] = p.getColor();
+    }
+
     private Pearl[] nextPearl() {
         // first (center) on bottom
         return new Pearl[] {new Pearl(2, 10, (int)Math.floor(Math.random() * 4)),
         new Pearl(2, 11, (int)Math.floor(Math.random() * 4))};
+    }
+    private void setNext() {
+        center = next.get(0)[0]; side = next.get(0)[1];
+        dir = 0; // side on top -> 0
+        active.add(center); active.add(side);
+        next.remove(0);
+        next.add(nextPearl());
+    }
+
+    private ArrayList<Vector2> getPops() {
+        return new ArrayList<Vector2> ();
     }
 
     public Board() {
@@ -47,13 +94,33 @@ public class Board {
         for (int i = 0; i < Constants.NEXT_SIZE; i ++) {
             next.add(nextPearl());
         }
+        setNext();
     }
 }
 
 class Pearl {
-    int x; float y;
-    float dy;
-    int color;
+    private int x; private float y;
+    private float dy;
+    private int color;
+
+    public float[] getXY() {
+        return new float[] {x, y};
+    }
+    public int getColor() {
+        return color;
+    }
+    public float getDy() {
+        return dy;
+    }
+
+    public void setDy(float n) {
+        dy = n;
+    }
+
+    public float accelerate(float gravity) {
+        dy += gravity;
+        return dy;
+    }
 
     public boolean update(int[][] pearls) {
         boolean collision = false;
--- core/src/main/java/com/bobapuyo/Constants.java
+++ core/src/main/java/com/bobapuyo/Constants.java
@@ -5,4 +5,7 @@ import com.badlogic.gdx.graphics.Color;
 public class Constants {
     final public static int COLORS = 4;
     final public static int NEXT_SIZE = 2;
+
+    final public static float GRAVITY = 2f; // cells per second
+    final public static float GRAV_ACC = 0.5f; // increase in gravity while in freefall
 }
